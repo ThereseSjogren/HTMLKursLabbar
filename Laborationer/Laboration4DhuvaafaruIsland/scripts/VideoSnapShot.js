d6df378e852a783400d16e65ec1f36f844b013a6
ThereseSjogren <nike_8@hotmail.com>
2016-06-01 12:19:13 +0200

    added a snapshot

---
+++ Laborationer/Laboration4DhuvaafaruIsland/scripts/VideoSnapShot.js
@@ -0,0 +1,8 @@
+﻿$(document).ready(function () {
+    $('#buttonSnapShot').on('click', snap);
+});
+function snap() {
+    var canvas = document.getElementById('myCanvas');
+    var videon = document.getElementById('myvideo');
+    canvas.getContext('2d').drawImage(videon, 0, 0, 400, 320);
+}
